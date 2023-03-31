eca525a6a84e42f27b039d6bdb93f293ea9a3423
pbh1112 <reujuilly@gmail.com>
2023-03-31 18:01:17 +0700

    commit

--- WebScrapingProject.py
+++ WebScrapingProject.py
@@ -1,43 +1,111 @@
 import requests
 from bs4 import BeautifulSoup
-
-
-url = "https://kenh14.vn/tiem-banh-ngot-an-kieng-noi-tieng-o-ha-noi-ngung-ban-sau-loat-lum-xum-nhan-vien-noi-dang-bao-tri-20230326170559079.chn"
-
-# http request method
-# - GET: lấy thông tin từ server
-# - POST: đẩy thông tin lên server nếu được phép
-
+from selenium import webdriver
+import pandas as pd
+from time import sleep
 
 def get_page_content(link):
     page_content = requests.get(link)
     soup = BeautifulSoup(page_content.text, "html.parser")
     return soup
 
+def page_analytics(soup):
+    fields = ["og:title", "og:description", "og:image"]
+    collected_data = {}
 
-# output: lấy đơcj title của bài báo này riêng
-# lấy được sapo (description) riêng
+    for field in fields:
+        for tag in soup.find_all("meta"):
+            if tag.get("property") == field:
+                collected_data[field] = tag.get("content")
+        
+    return collected_data
 
+# bw1: đưa tất cả title, description, og:image vào dictionary
+def hw1():
+    print(f"\n---------------\nHW1: Add swapped data to new dictionary")
 
-def get_page_title(soup):
-    page_title = soup.find_all("title")
-    print(page_title)
+    url = "https://kenh14.vn/tiem-banh-ngot-an-kieng-noi-tieng-o-ha-noi-ngung-ban-sau-loat-lum-xum-nhan-vien-noi-dang-bao-tri-20230326170559079.chn"
+    soup = get_page_content(url)
+    collected_data = page_analytics(soup)
 
+    for key, value in collected_data.items():
+        print(f"\n- {key}: {value}")
+    
+    sleep(1)
 
-def get_page_something(soup):
-    page_something = soup.find("meta", attrs={"property": "og:image"})
-    print(page_something)
+# bw2: lấy toàn bộ link trong 1 chuyên mục trên kenh14
+def hw2():
+    print(f"\n---------------\nHW2: Collect all article links in a category on Kenh14")
 
+    url = "https://kenh14.vn/musik/au-my.chn"
+    print(f"* Category Link: {url}\n")
+
+    soup = get_page_content(url)
+    titles = soup.find_all('h3', class_ = "knswli-title")
+
+    links = []
+    for link in titles:
+        result = link.find('a').attrs["href"]
+        links.append(result)
+
+        print(f"{titles.index(link) + 1}: {result}\n")
+
+    sleep(1)
+
+# bw3: lấy toàn bộ các sản phẩm trong 10 trang đầu tiên trong daily discovery Shopee. Lấy title và giá sản phẩm
+def hw3():
+    print(f"\n---------------\nHW1: Add swapped data to new dictionary\n")
+
+    nums = int(input("* Number of page to crawl: "))
+    for num in range(1,nums + 1):
+
+        # build shopee url
+        url = (f"https://shopee.vn/daily_discover?pageNumber={str(num)}")
+        print(f"\n----------\nPage {num}: {url}")
+
+        # open chrome browser by webdriver
+        driver = webdriver.Chrome()
+        driver.get(url)
+        sleep(5)
+
+        # load page source into the program
+        page_source = BeautifulSoup(driver.page_source, "html.parser")
+            
+        # get all products in this "Daily Discover" page
+        parents_tags = page_source.find_all("div", class_ = 'tIJtcv row')
+
+        for result in parents_tags: 
+            name_result = result.find_all("div", class_ = "ie3A+n Cve6sh")
+            price_result = result.find_all("span", class_ = "juCMSo")
+            solds_result = result.find_all("div", class_ = "r6HknA dQAXj1")
+
+        list_product_names = []
+        list_product_price = []
+        list_product_solds = []
+
+        for item in range(len(name_result)):
+            # extract text in div . tag
+            name_item = name_result[item].text
+            price_item = price_result[item].text
+            solds_item = solds_result[item].text
+                
+            list_product_names.append(name_item)
+            list_product_price.append(price_item)
+            list_product_solds.append(solds_item)
+            item = item + 1
+            
+        df = pd.DataFrame()
+        df["name"] = list_product_names
+        df["price"] = list_product_price
+        df["solds"] = list_product_solds
+        print(df)
+
+        num += 1         
 
 def main():
-    soup = get_page_content(url)
-    get_page_something(soup)
-
+    hw1()
+    hw2()
+    hw3()
 
 if __name__ == "__main__":
     main()
-
-
-# bw1: đưa tất cả title, description, og:image vào dictionary
-# bw2: lấy toàn bộ link trong 1 chuyên mục trên kenh14
-# bw3: lấy toàn bộ các sản phẩm trong 10 trang đầu tiên trong daily discovery Shopee. Lấy title và giá sản phẩm
--- draft.py
+++ draft.py
@@ -1,78 +1,17 @@
-import random
+from selenium import webdriver
+
+def get_chrome_driver():
+    chrome_options = webdriver.ChromeOptions() 
+    
+    # Keeps the browser open
+    chrome_options.add_experimental_option("detach", True)
+    
+    return webdriver.Chrome(options = chrome_options)
 
 
-def hw2():
-    print("\nHw2: Battleship")
 
-    def gen_list(n):
-        list = []
-        for i in range(n):
-            x = random.randint(1, 30)
-            list.append(x)
-        return list
-
-    def input_int_1_7(mess):
-        while True:
-            x = input(mess)
-            if not x.isdigit() or int(x) < 1 or int(x) > 7:
-                print("* Vui lòng nhập số nguyên từ 1 đến 7!")
-            else:
-                return int(x)
-
-    def input_int_1_30(mess):
-        while True:
-            x = input(mess)
-            if not x.isdigit() or int(x) < 1 or int(x) > 30:
-                print("* Vui lòng nhập số nguyên từ 1 đến 30!")
-            else:
-                return int(x)
-
-    so_tau = input_int_1_7("Số lượng tàu cấp cho mỗi người chơi (1-7): ")
-    lst_1 = gen_list(so_tau)
-    lst_2 = gen_list(so_tau)
-    round = 1
-    score_1 = 0
-    score_2 = 0
-
-    print(
-        f"\nĐiểm danh tàu của hai người chơi:\n- Người chơi 1: {lst_1}\n- Người chơi 2: {lst_2}"
-    )
-
-    while round <= 3 or round <= so_tau:
-        print(f"\nRound {round}:")
-        user_1_turns = input_int_1_30("- Lượt của người chơi 1 (1-30): ")
-        user_2_turns = input_int_1_30("- Lượt của người chơi 2 (1-30): ")
-
-        if user_1_turns in lst_2:
-            score_1 += 1
-            lst_2.remove(user_1_turns)
-        if user_2_turns in lst_1:
-            score_2 += 1
-            lst_1.remove(user_2_turns)
-
-        print(
-            f"-> [Round {round}] Điểm số của người chơi 1 và 2 là {score_1} : {score_2}"
-        )
-        round += 1
-
-    if score_1 < score_2:
-        print(
-            f"\n[Game Over] Người chơi 2 chiến thắng. Tỉ số trung cuộc: {score_1} : {score_2}\n"
-        )
-    elif score_1 > score_2:
-        print(
-            f"\n[Game Over] Người chơi 1 chiến thắng. Tỉ số trung cuộc: {score_1} : {score_2}\n"
-        )
-    elif score_1 == score_2:
-        print(
-            f"\n[Game Over] Hai người chơi hoà nhau. Tỉ số trung cuộc: {score_1} : {score_2}\n"
-        )
-
-
-def main():
-    hw2()
-    return
-
-
-if __name__ == "__main__":
-    main()
+# Gets our chrome driver and opens our site
+chrome_driver = get_chrome_driver()
+chrome_driver.get("https://www.google.com/")
+print('The browser should not close after you see this message')
+chrome_driver.service.stop()
